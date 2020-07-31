640e5d6ae1d1107a02426e2e44c4e5a831befa9d
Daniel <daniel_jaros@wp.pl>
2020-07-31 16:58:37 +0200

    Add back to TOP button to index.html file

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ a.innerHTML = 'Działa';
 $(document).ready(function() {
     $('.hamburgerMenu--toggle').click(function() {
         $('nav').toggleClass('menu__navigation__bar__active')
+        $('.hamburgerMenu--bars').toggleClass('active')
 
     })
     $('.in-products-btn').click(function() {
@@ -84,17 +85,33 @@ function checkPosition() {
         $('nav ul li .in-products-btn').attr('href', 'produkty-do-wewnatrz')
         $('nav ul li .out-products-btn').attr('href', 'produkty-na-zewnatrz')
         $('nav ul li .liberon-products-btn').attr('href', 'liberon')
+
+        // Addin active class to menu element of active page
+        $('nav ul li').click(function() {
+            $(this).addClass("active").siblings().removeClass("active");
+        })
     }
 }
 var x = window.matchMedia('(max-width:865px)')
 checkPosition()
 x.addListener(checkPosition)
 
-// Addin active class to menu element of active page
-/* $('nav ul li').click(function() {
-    $(this).addClass("active").siblings().removeClass("active");
-})
-*/
+/*Arrow UP display after scroll down */
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+    $("#topBtn").click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 400);
+    });
+});
+
 
 
 // Closing hamburgerMenu when click out of menu__navigationBar DIV !!!NIEDOKOŃCZONE!!!!
